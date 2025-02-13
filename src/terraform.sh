55641ee3ceece666946d6c997ac8da1197d09f0e
giuseppesiviero <giu.siviero@gmail.com>
2025-02-13 11:49:55 +0100

    add terraform.sh

---
+++ src/terraform.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# https://developer.hashicorp.com/terraform/install
+
+echo "Aggiornamento dei pacchetti e installazione delle dipendenze necessarie..."
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+
+echo "Aggiunta della chiave GPG di HashiCorp..."
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+
+echo "Aggiunta del repository di HashiCorp alle sorgenti di APT..."
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+echo "Aggiornamento dei pacchetti e installazione di Terraform..."
+sudo apt-get update && sudo apt-get install terraform
+
+echo "Configurazione dell'autocompletamento per Terraform..."
+touch ~/.bashrc
+terraform -install-autocomplete
+
+echo "Puoi verificare l'installazione eseguendo il comando 'terraform -version'."
+terraform -version
+
+echo "Installazione di Terraform completata con successo!"
