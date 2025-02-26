29014e4731579e34f63bd1d412627ccf8d986fe6
giuseppesiviero <giu.siviero@gmail.com>
2025-02-26 09:25:20 +0100

    refactor aws.sh

--- src/aws.sh
+++ src/aws.sh
@@ -2,32 +2,32 @@
 
 # https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
 
-# Inizio installazione di AWS CLI
-echo "Iniziamo l'installazione di AWS CLI, un insieme di strumenti di comando per gestire le risorse di Amazon Web Services direttamente dal terminale."
+# Begin AWS CLI installation
+echo "Starting AWS CLI installation, a set of command-line tools for managing Amazon Web Services resources directly from the terminal."
 
-# 1. Installare unzip:
-echo "Installazione di unzip..."
+# 1. Install unzip:
+echo "Installing unzip..."
 sudo apt-get install -y unzip
 
-# 2. Scaricare il pacchetto AWS CLI:
-echo "Scaricamento del pacchetto AWS CLI..."
+# 2. Download the AWS CLI package:
+echo "Downloading the AWS CLI package..."
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 
-# 3. Decomprimere il pacchetto:
-echo "Decompressione del pacchetto AWS CLI..."
+# 3. Unzip the package:
+echo "Unzipping the AWS CLI package..."
 unzip awscliv2.zip
 
-# 4. Installare AWS CLI:
-echo "Installazione di AWS CLI..."
+# 4. Install AWS CLI:
+echo "Installing AWS CLI..."
 sudo ./aws/install
 
-# 5. Verificare l'installazione:
-echo "Verifica della versione di AWS CLI..."
+# 5. Verify the installation:
+echo "Verifying AWS CLI version..."
 aws --version
 
-# 6. Rimuovere il file ZIP:
-echo "Rimozione del file awscliv2.zip..."
+# 6. Remove the ZIP file:
+echo "Removing awscliv2.zip file..."
 rm -f awscliv2.zip
 
-# Fine installazione
-echo "Installazione di AWS CLI completata!"
+# End installation
+echo "AWS CLI installation completed!"
