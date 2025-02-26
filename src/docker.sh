fa261b4d7d12a77eee485707094aed06d1dd071c
giuseppesiviero <giu.siviero@gmail.com>
2025-02-26 09:37:55 +0100

    refactor docker.sh

--- src/docker.sh
+++ src/docker.sh
@@ -2,36 +2,44 @@
 
 # https://docs.docker.com/engine/install/ubuntu/
 
-echo "Installazione Docker CLI"
+echo "Installing Docker CLI"
 
-echo "Aggiornamento dei pacchetti e installazione delle dipendenze necessarie..."
+# Update packages and install necessary dependencies
+echo "Updating packages and installing necessary dependencies..."
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg
 
-echo "Creazione della directory per le chiavi GPG..."
+# Create directory for GPG keys
+echo "Creating directory for GPG keys..."
 sudo install -m 0755 -d /etc/apt/keyrings
 
-echo "Scaricamento e configurazione della chiave GPG ufficiale di Docker..."
+# Download and configure the official Docker GPG key
+echo "Downloading and configuring the official Docker GPG key..."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-echo "Aggiunta del repository di Docker alle sorgenti di APT..."
+# Add Docker repository to APT sources
+echo "Adding Docker repository to APT sources..."
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $($(. /etc/os-release && echo $VERSION_CODENAME)) stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-echo "Aggiornamento dei pacchetti..."
+# Update packages
+echo "Updating packages..."
 sudo apt-get update
 
-echo "Installazione di Docker e dei suoi componenti..."
+# Install Docker and its components
+echo "Installing Docker and its components..."
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-echo "Creazione del gruppo Docker e aggiunta dell'utente corrente..."
+# Create Docker group and add the current user
+echo "Creating Docker group and adding the current user..."
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 
-echo "Installazione di Docker completata con successo!"
-echo "Puoi verificare lo stato del servizio Docker con 'sudo systemctl status docker' ed eseguire un container di prova con 'docker run hello-world'."
+# Installation completed
+echo "Docker installation completed successfully!"
+echo "You can verify the Docker service status with 'sudo systemctl status docker' and run a test container with 'docker run hello-world'."
 
